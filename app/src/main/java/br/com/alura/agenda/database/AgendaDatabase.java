986f1f025aa2ccd2cc39db17fa3a8fbda9006418
Claudio Costa <claudio@imaginaction.com.br>
2019-10-07 08:06:20 -0300

    feet: 02 - 03 - Implementando componentes principais do Room Parte 2

--- app/src/main/java/br/com/alura/agenda/database/AgendaDatabase.java
+++ app/src/main/java/br/com/alura/agenda/database/AgendaDatabase.java
@@ -1,8 +1,11 @@
 package br.com.alura.agenda.database;
 
 import androidx.room.Database;
+import androidx.room.RoomDatabase;
 
-@Database(entities = {}, version = 1)
-public class AgendaDatabase  {
+import br.com.alura.agenda.model.Aluno;
+
+@Database(entities = {Aluno.class}, version = 1, exportSchema = false)
+public  abstract class AgendaDatabase extends RoomDatabase {
 
 }
