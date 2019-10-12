e084ea322d2ff12803d40158c24008c1551d788a
Claudio Costa <claudio@imaginaction.com.br>
2019-10-12 11:31:20 -0300

    feet: 03 - 03 - Adicionando campo novo

--- app/src/main/java/br/com/alura/agenda/model/Aluno.java
+++ app/src/main/java/br/com/alura/agenda/model/Aluno.java
@@ -13,6 +13,7 @@ public class Aluno implements Serializable {
     @PrimaryKey(autoGenerate = true)
     private int id = 0;
     private String nome;
+    private String sobrenome;
     private String telefone;
     private String email;
 
@@ -67,5 +68,17 @@ public class Aluno implements Serializable {
 
     public boolean temIdValido() {
         return id > 0;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getNomeCompleto() {
+        return  nome + " " + sobrenome;
     }
 }
